9f37c417c02f036d93026bfdc4dfc52a42193494
luciobo <luciobo@icloud.com>
2023-01-19 16:14:41 +0100

    fatto secondo step

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('Home');
+
+    $name = "Lucio";
+    $surname = "Bo";
+
+    return view('Home', [
+        "name" => $name,
+        "surname" => $surname,
+    ]);
 });
